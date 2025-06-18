97f01e0353d518ca885fcaf36e6d6ce91c5d2903
meenakshi-mittal <95123915+meenakshi-mittal@users.noreply.github.com>
2025-06-18 10:49:24 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,13 @@
 eval "$(conda shell.bash hook)"
 conda activate askademia
 
+# Kill any process currently using port 5000
+PID=$(lsof -ti tcp:5000)
+if [ -n "$PID" ]; then
+  echo "Killing process on port 5000 (PID: $PID)"
+  kill -9 $PID
+fi
+
 # Open new terminal to launch browser
 osascript <<EOF
 tell application "Terminal"
